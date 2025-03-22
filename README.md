1d77cd080591a3e88223df0324f79e3c598ca302
Omar <omareegab@gmail.com>
2025-03-22 14:42:13 -0500

    test

---
+++ README.md
@@ -0,0 +1,13 @@
+Primero
+    - Necesito que mi app trackee mi entrenamiento diario (ej)
+        Lunes 18/03
+            pull up    bw   5 series    8 rep   6 rps  6 rps  4 rps  2rps
+            exercise   kg    sets        reps
+
+me deje entender
+
+
+por ahora eso
+
+
+vamonooooo
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\WorkoutController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/workouts', [WorkoutController::class, 'index'])->name('workouts.index');
+    Route::post('/workouts', [WorkoutController::class, 'store'])->name('workouts.store');
+});
+
+
+require __DIR__ . '/auth.php';
