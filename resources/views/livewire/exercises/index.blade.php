3a8f81230b39f157fa629ce5a8342b908d1e90f0
Omar <saucyfunnel@gmail.com>
2025-04-10 22:17:47 -0500

    test

--- resources/views/livewire/exercises/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h1>Lista de Ejercicios</h1>
-    </x-slot>
-    <livewire:exercises />
-
-</x-app-layout>
--- resources/views/livewire/programs/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h1>Tu Registro de Entrenamiento</h1>
-    </x-slot>
-    <livewire:programs />
-
-</x-app-layout>
